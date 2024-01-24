1a053f986b32cae4df1da7f2669c32d823d7225c
Guilherme Leme <guih.fleme@gmail.com>
2024-01-24 11:15:45 -0300

    :sparkles: feat: add search bar

--- src/view/pages/Home.tsx
+++ src/view/pages/Home.tsx
@@ -5,12 +5,15 @@ import { FaCarSide, FaMotorcycle, FaVanShuttle, FaSquareYoutube , FaInstagram } 
 import { MdOutlinePedalBike } from "react-icons/md";
 
 
-import animationData from '../../assets/menu-animation3.json'
+import menuData from '../../assets/menu-animation3.json'
+import globeData from '../../assets/globe-animation.json'
 import Logo from '../../assets/logo.png'
 import Bruna from '../../assets/bruna.jpg'
+import Higor from '../../assets/higor.jpg'
 
 export function Home() {
   const menuRef = useRef<LottieRefCurrentProps>(null)
+  const globeRef = useRef<LottieRefCurrentProps>(null)
 
   useEffect(() => {
     setTimeout(() => {
@@ -32,7 +35,7 @@ export function Home() {
             id='menu'
             className='w-[48px] opacity-0 hover:cursor-pointer'
             lottieRef={menuRef}
-            animationData={animationData}
+            animationData={menuData}
             onClick={() => {
               if(menuRef.current?.getDuration(true) === 70) {
                 menuRef.current?.playSegments([70,139], true)
@@ -79,10 +82,10 @@ export function Home() {
             </li>
           </ul>
         </nav>
-        <h3 className='font-bold text-3xl text-[#80b918] uppercase'>Para os amantes da estrada</h3>
+        <h1 className='text-[#80b918] text-5xl font-bold uppercase'>Para os amantes da estrada</h1>
       </div>
       {/* texts */}
-      <section className='flex items-center justify-center gap-12 p-8  w-[1440px] mx-auto'>
+      <section className='flex items-center justify-center gap-12 p-8  w-[1440px] mx-auto bg-[#81b9181f] m-4'>
         <div className='border-[1px] rounded-md border-[#80b918] p-4'>
           <h1 className='font-bold text-slate-700 text-xl mb-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit</h1>
           <p className='text-gray-900'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae eos voluptas perferendis nam eligendi quas praesentium. Deleniti, earum delectus dicta repudiandae, necessitatibus nesciunt unde repellendus expedita accusantium placeat distinctio ratione?
@@ -100,16 +103,41 @@ export function Home() {
         </div>
       </section>
       {/* search */}
-      <div>
-
+      <div className='width-[1440px] mx-auto flex items-center justify-center gap-4'>
+        <h3>Eu vou para</h3>
+        <input
+          type="text"
+          placeholder='pesquisar'
+          className='border-[1px] border-[#80b918] rounded-md p-2 w-[360px]'
+        />
+        <Lottie
+            className='w-[72px] '
+            lottieRef={globeRef}
+            animationData={globeData}
+            autoplay={true}
+            loop={false}
+            onComplete={() => {
+              globeRef.current?.playSegments([178, 420], true)
+            }}
+          />
       </div>
       {/* who we are */}
-      <section>
-        <h1>Quem somos nós?</h1>
-        <a href='https://www.instagram.com/brunagrybogi/' className='flex flex-col gap-4'>
-          <img src={Bruna} alt="" className='w-60 h-60 object-cover rounded-full border-8 border-[#80b918]'/>
-          <h3 className='text-3xl font-bold text-[#80b918]'>Bruna Grybogi</h3>
-        </a>
+      <section className='flex flex-col items-center justify-center gap-8'>
+        <h1 className='text-[#80b918] text-6xl font-bold'>Quem somos nós?</h1>
+        <div className='flex items-center justify-center space-x-32 p-8'>
+          <a href='https://www.instagram.com/brunagrybogi/' className='flex flex-col gap-4 items-center justify-center '>
+            <img src={Bruna} alt="" className='w-60 h-60 object-cover rounded-full border-8 border-[#80b918] hover:scale-105'/>
+            <h3 className='text-3xl font-bold text-[#80b918]'>Bruna Grybogi</h3>
+          </a>
+          <a href='https://www.instagram.com/brunagrybogi/' className='flex flex-col gap-4 items-center justify-center'>
+            <img src={Higor} alt="" className='w-60 h-60 object-cover rounded-full border-8 border-[#80b918]'/>
+            <h3 className='text-3xl font-bold text-[#80b918]'>Higor Carneiro</h3>
+          </a>
+          <a href='https://www.instagram.com/brunagrybogi/' className='flex flex-col gap-4 items-center justify-center'>
+            <img src={Bruna} alt="" className='w-60 h-60 object-cover rounded-full border-8 border-[#80b918]'/>
+            <h3 className='text-3xl font-bold text-[#80b918]'>Colaborador</h3>
+          </a>
+        </div>
       </section>
       {/* footer */}
       <footer className='bg-[#80b918] text-white h-auto'>
@@ -119,13 +147,13 @@ export function Home() {
             <div className='flex flex-col items-center justify-center text-xl gap-6 flex-1'>
               <div className='flex flex-col items-start justify-center text-xl gap-1 translate-x-[14px]'>
                 <span>Contato:</span>
-                <a href="mailto:ola@hodophile.com" className='text-'>ola@hodophile.com</a>
+                <a href="mailto:ola@hodophile.com" className='hover:text-gray-200 transition-all duration-500'>ola@hodophile.com</a>
               </div>
               <div id="contacts" className='flex items-center justify-center flex-1 gap-8'>
                 <a href="https://www.youtube.com/@TheSunsetRiders">
                   <FaSquareYoutube  className='text-[60px] hover:text-gray-200 hover:scale-105 transition-all duration-500' />
                 </a>
-                <a href="https://www.youtube.com/@TheSunsetRiders">
+                <a href="https://www.instagram.com/somoshodophiles">
                   <FaInstagram className='text-[62px] hover:text-gray-200 hover:scale-105 transition-all duration-500' />
                 </a>
               </div>
